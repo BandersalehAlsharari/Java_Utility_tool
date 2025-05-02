a0cc278fe3e71eaffc29a64e6886cfa413a90323
BandersalehAlsharari <i20f108@gmail.com>
2025-05-02 03:40:39 +0300

    multiply feature added

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -14,9 +14,15 @@ public class AddTwoNumbers {
 
         // Add the two numbers
         int sum = num1 + num2;
-
-        // Display the result
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        // Subtract the two numbers
+        int difference = num1 - num2;
+        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + difference);
+
+        // Multiply the two numbers
+        int product = num1 * num2;
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
 
         // Close the scanner
         scanner.close();
